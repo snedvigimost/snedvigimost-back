0511eee1cc0df2f80da1202d1bb11c7f4bb0029e
Andriy Orehov <andriyorehov@gmail.com>
2020-10-10 22:32:05 +0300

    add total_pages to paginated_response

--- realestate/listings/views.py
+++ realestate/listings/views.py
@@ -2,6 +2,7 @@ from django_filters import rest_framework as filters
 from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework_extensions.decorators import paginate
 
 from .models import Listing, ProductFilter
@@ -11,6 +12,17 @@ from .serializers import ListingSerializer
 class StandardResultsSetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
 
 
 @paginate(pagination_class=StandardResultsSetPagination, ordering='-created_at')
